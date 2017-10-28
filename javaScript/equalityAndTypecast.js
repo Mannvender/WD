6c983b873456fbb9bd12b0169fdfd9634793659a
RoBo_ <mannvender001@gmail.com>
2017-10-28 10:30:53 +0530

    equality and typecast {} and [] examples added
    need to learn these

--- javaScript/equalityAndTypecast.js
+++ javaScript/equalityAndTypecast.js
@@ -11,3 +11,15 @@ console.log(0 == "")//true
 //unary + operator
 console.log("1")
 console.log(+"1")
+//on running one by one first one will return "1" and second will try to typecast to number 1
+let o = {};
+console.log(o.toString())
+
+// "" + {} [object Object]
+
+//{} in LHS is block and in RHS is object
+
+// [] + {} - gives [object Object]  (on running line by line)
+// {} + [] - gives 0  (because here {} is considered block and in above case it was object
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
