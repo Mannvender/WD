a73a93069a715d77927eb27245b332b50394f5db
RoBo_ <mannvender001@gmail.com>
2017-11-12 14:38:56 +0530

    Assignment done - read and write using NodeJS
    read from one file and write to other file, make sure you write - (write file) code in call back of read file code as these functions are async in nature

--- nodejs/fileOps/readFile.js
+++ nodejs/fileOps/readFile.js
@@ -1,7 +1,44 @@
 const fs = require('fs')
-fs.readFile('./myfile.txt', function (err, data) {
+/*fs.readFile('./myfile.txt', function (err, data) {
     if (err) throw err
 
     console.log(data)
     console.log(data.toString())
+})*/
+let toPrint = "";
+fs.readFile('./input.txt', function (err, data) {
+    if (err) throw err
+
+
+    // console.log(data)
+    let input = data.toString();
+    console.log(data.toString())
+    let arr = input.split('\n')
+    console.log(arr)
+    // let toPrint = "";
+    for (let a = 0; a < arr.length; a++) {
+        let count = 0;
+        for (let b = a + 1; b < arr.length; b++) {
+            if (arr[a] === arr[b]) {
+                delete arr[b]
+                count++;
+            }
+        }
+        if (arr[a] != undefined) {
+            toPrint = toPrint + arr[a] + ":" + (count + 1) + "\n"
+        }
+
+    }
+    console.log(toPrint + "\n *****test******")
+
+    fs.writeFile('output.txt', toPrint.toString(), function (err) {
+        if (err) throw err
+
+        //acts as else block
+        console.log("output file was written")
+    })
+
 })
+
+console.log("*********************")
+
--- nodejs/fileOps/writeFile.js
+++ nodejs/fileOps/writeFile.js
@@ -2,11 +2,19 @@
 //we are using fileSystem api
 
 const fs = require('fs')
+
 // __dirname
 fs.writeFile('myfile.txt', "some data", function (err) {
     if (err) throw err
 
-
     //acts as else block
     console.log("file was written")
+})
+
+
+fs.writeFile('input.txt', "cat\nrat\nhuman\nduck\ncat\ndog\ncat", function (err) {
+    if (err) throw err
+
+    //acts as else block
+    console.log("input file was written")
 })
